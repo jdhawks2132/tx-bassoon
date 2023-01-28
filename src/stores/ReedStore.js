ec0d9a114e055c5e256fbdb5a6e6c0c8b99a7387
Joshua Hawks <jdhawks@gmail.com>
2023-01-28 17:02:48 -0600

    update reed photos in ReedStore

--- src/stores/ReedStore.js
+++ src/stores/ReedStore.js
@@ -5,7 +5,8 @@ const ReedStore = writable([
 		id: 1,
 		name: 'Texas Reed',
 		price: '$30.00',
-		image: 'https://images.unsplash.com/photo-15004',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/reed1.jpg?raw=true',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel',
 		quantity: 1,
 		likes: 0,
@@ -15,7 +16,8 @@ const ReedStore = writable([
 		id: 2,
 		name: 'Holiday Reed',
 		price: '$30.00',
-		image: 'https://images.unsplash.com/photo-15004',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/reed2.jpg?raw=true',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euis',
 		quantity: 1,
 		likes: 0,
@@ -25,7 +27,8 @@ const ReedStore = writable([
 		id: 3,
 		name: 'Student Reed',
 		price: '$20.00',
-		image: 'https://images.unsplash.com/photo-15004',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/reed1.jpg?raw=true',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 		quantity: 1,
 		likes: 0,
