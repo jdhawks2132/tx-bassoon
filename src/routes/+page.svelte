17d1990f049775268db7bdfdf68ce03a6d0f57c0
Joshua Hawks <jdhawks@gmail.com>
2023-01-28 17:47:31 -0600

    contact form mock up added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,6 +26,12 @@
 		shipping: string;
 	}[];
 
+	let formState = {
+		name: '',
+		email: '',
+		message: ''
+	};
+
 	BassoonStore.subscribe((data) => {
 		bassoons = data;
 	});
@@ -33,6 +39,19 @@
 	ReedStore.subscribe((data) => {
 		reeds = data;
 	});
+
+	const handleSubmit = () => {
+		console.log(formState);
+
+		alert(
+			`Thank you for your message, ${formState.name}! We will get back to you as soon as possible.`
+		);
+		formState = {
+			name: '',
+			email: '',
+			message: ''
+		};
+	};
 </script>
 
 <!-- add meta data like title svelte -->
@@ -121,7 +140,54 @@
 	</div>
 </section>
 
-<section id="contact" class="mt-20 h-full">
-	<h2 class="text-6xl font-bold text-center my-20">Contact</h2>
-	<div class="flex flex-col lg:flex-row items-center justify-center" />
+<section id="contact" class="pb-20 h-full">
+	<h2 class="text-6xl font-bold text-center mb-20">Contact</h2>
+	<div class="flex flex-col lg:flex-row items-center justify-center">
+		<form
+			on:submit|preventDefault={handleSubmit}
+			class="flex flex-col w-11/12 lg:w-2/3 mx-auto py-8 bg-white rounded-md shadow-md"
+		>
+			<div class="flex flex-col lg:flex-row items-center justify-center">
+				<div class="flex flex-col w-11/12 lg:w-1/3 mx-auto">
+					<label for="name" class="text-xl font-medium">Name</label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						class="border-gray-300 border-2 rounded-md px-4 py-2 my-2"
+						bind:value={formState.name}
+					/>
+				</div>
+				<div class="flex flex-col w-11/12 lg:w-1/3 mx-auto">
+					<label for="email" class="text-xl font-medium">Email</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						class="border-gray-300 border-2 rounded-md px-4 py-2 my-2"
+						bind:value={formState.email}
+					/>
+				</div>
+			</div>
+			<div class="flex flex-col w-11/12 mx-auto">
+				<label for="message" class="text-xl font-medium">Message</label>
+				<textarea
+					name="message"
+					id="message"
+					cols="30"
+					rows="10"
+					class="border-gray-300 border-2 rounded-md px-4 py-2 my-2"
+					bind:value={formState.message}
+				/>
+			</div>
+			<div class="flex flex-col w-11/12 mx-auto">
+				<button
+					type="submit"
+					class="bg-accent text-white px-6 py-3 m-2 w-52 rounded-md text-lg text-center font-medium hover:bg-accentDark"
+				>
+					Submit
+				</button>
+			</div>
+		</form>
+	</div>
 </section>
