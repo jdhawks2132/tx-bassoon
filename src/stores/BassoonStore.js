114a297068dd34907d1db6615b0bfe222428da63
Joshua Hawks <jdhawks@gmail.com>
2023-01-28 13:59:50 -0600

    update img src string to gh hosting

--- src/stores/BassoonStore.js
+++ src/stores/BassoonStore.js
@@ -4,28 +4,32 @@ const BassoonStore = writable([
 	{
 		id: '214',
 		name: 'Model 214',
-		image: '/src/lib/assets/images/bassoon.jpg',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/bassoon.jpg?raw=true',
 		price: 'Call for price',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel'
 	},
 	{
 		id: '215',
 		name: 'Model 215',
-		image: '/src/lib/assets/images/bassoon.jpg',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/bassoon.jpg?raw=true',
 		price: 'Call for price',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel'
 	},
 	{
 		id: '216',
 		name: 'Model 216',
-		image: '/src/lib/assets/images/bassoon.jpg',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/bassoon.jpg?raw=true',
 		price: 'Call for price',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel'
 	},
 	{
 		id: '217',
 		name: 'Model 217',
-		image: '/src/lib/assets/images/bassoon.jpg',
+		image:
+			'https://github.com/jdhawks2132/tx-bassoon/blob/main/src/lib/assets/images/bassoon.jpg?raw=true',
 		price: 'Call for price',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel'
 	}
